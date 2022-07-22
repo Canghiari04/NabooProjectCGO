68d4a078668cc613c7fbfff221c17d5930fb2e4f
macbookpro <macbookpro@192.168.1.7>
2022-07-22 18:35:31 +0200

    Implementation class Utente and Amministratore

---
+++ NabooProject/src/application/Amministratore.java
@@ -0,0 +1,20 @@
+package application;
+
+public class Amministratore extends Utente implements IAmministratore{
+
+	
+	public Amministratore(String nickName, String password)
+	{
+		super(nickName, password);
+	}
+	
+	public String getNickName()
+	{
+		return this.nickName;
+	}
+	
+	public String getPassword()
+	{
+		return this.password;
+	}
+}
---
+++ NabooProject/src/application/Utente.java
@@ -0,0 +1,17 @@
+package application;
+
+public abstract class  Utente {
+
+	protected String nickName;
+	protected String password;
+	
+	public Utente(String nickName, String password)
+	{
+		this.nickName = nickName;
+		this.password = password;
+	}
+	
+	public abstract String getNickName();
+	
+	public abstract String getPassword();
+}
