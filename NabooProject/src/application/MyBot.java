90a7bcc15519ca079a9eec185cfd1d9ae9941128
macbookpro <macbookpro@192.168.1.16>
2022-07-23 17:02:53 +0200

    Plus by MyBot, part of code for login

--- NabooProject/src/application/MyBot.java
+++ NabooProject/src/application/MyBot.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.*;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -7,19 +8,42 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyBot extends TelegramLongPollingBot 
 {
+	final Scanner scan = new Scanner(System.in);
+	final Map<String, String> dictonaryUtente = new TreeMap<String, String>();
 
 	@Override
 	public void onUpdateReceived(Update update) {
 	    // We check if the update has a message and the message has text
 	    if (update.hasMessage() && update.getMessage().hasText()) {
-	        SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-	        message.setChatId(update.getMessage().getChatId().toString());
-	        message.setText(update.getMessage().getText());
+	        SendMessage response = new SendMessage(); // Create a SendMessage object with mandatory fields
 	        
-	        System.out.println(update.getMessage().getText());
+	        response.setChatId(update.getMessage().getChatId().toString());
+	        response.setText(update.getMessage().getText());
+	        	  
+	        // Trovare il modo per equals("differenti funzionalita'") del bot Telegram
+	        
+	        if(response.equals("/accedi"))
+	        {
+	        	response.setText("Inserisci il tuo nick name");
+	        	response.getText();
+	        	
+	        	String nickName = scan.nextLine();
+	        	String password = scan.nextLine();
+	        	
+	        	if(dictonaryUtente.containsValue(password) && dictonaryUtente.containsKey(nickName))
+	        	{
+	        		
+	        	}
+	        	else
+	        	{
+	        		registrazione(nickName, password);
+	        	}
+	        	
+	        }
+	        
 	        
 	        try {
-	            execute(message); // Call method to send the message
+	            execute(response); // Call method to send the message
 	        } catch (TelegramApiException e) {
 	            e.printStackTrace();
 	        }
@@ -34,5 +58,14 @@ public class MyBot extends TelegramLongPollingBot
     public String getBotToken() {
         // TODO
         return "5471762884:AAGeRCek_JkVklyP7kYtTYwKL2Xio0ZtpfI";
+    }
+    
+    public boolean registrazione(String nickName, String password)
+    {
+    	dictonaryUtente.put(nickName, password);
+    	
+    	Client c = new Client(nickName, password);
+    	
+    	// Client da aggiungere in qualche struttura dati per la successiva stampa nel file
     }
 }
